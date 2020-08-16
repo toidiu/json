b3df0827d241b969428e0ed9e3888e08f045d4ef
toidiu <apoorv@toidiu.com>
2020-08-15 22:17:37 -0700

    test and parse space early

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,14 @@ mod json_serde;
 use nom::{
     branch::alt,
     bytes::complete::is_not,
-    error::{context, convert_error, ErrorKind, ParseError,VerboseError},
-    bytes::complete::{tag_no_case, take_while_m_n, take_while},
+    bytes::complete::{tag_no_case, take_while, take_while_m_n},
     character::complete::char,
     combinator::map_res,
-    combinator::{map, opt, cut},
-    number::complete::{le_i32, le_u32},
+    combinator::{cut, map, opt},
+    error::{context, convert_error, ErrorKind, ParseError, VerboseError},
+    number::complete::double,
     sequence::delimited,
+    sequence::preceded,
     sequence::tuple,
     IResult,
 };
@@ -33,41 +34,30 @@ pub struct JsonParser<'a> {
 }
 
 impl<'a> JsonParser<'a> {
-    pub fn parse(mut json: &'a str) -> Result<Self, JsonError> {
-        let mut bytes: &[u8] = json.as_bytes();
-        let mut res: Vec<Value> = Vec::new();
-        while bytes.len() > 0 {
-            let (temp_json, temp_res) = parse_bool(bytes)
-                .map(|(rest, res)| (rest, Value::Bool(res)))
-                .or_else(|e| Err(JsonError::ParseError(e.to_string())))?;
-
-            bytes = temp_json;
-            res.push(temp_res);
-        }
-        println!("{:?}", res);
-        println!("{} {:?}", str::from_utf8(bytes).unwrap(), res);
-        Ok(JsonParser {
-            input: bytes,
-            output: res,
-        })
+    pub fn parse(mut json: &'a str) -> IResult<&'a str, bool> {
+        preceded(parse_space, parse_bool)(json)
     }
 }
 
-fn space<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
+fn parse_space<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
     let chars = " \t\r\n";
     take_while(move |c| chars.contains(c))(i)
 }
 
-fn parse_num(input: &[u8]) -> IResult<&[u8], i32> {
-    // alt((le_i32, le_u32))(input)
-    le_i32(input)
-    // .map_err(|e| JsonError::ParseError(ErrorDetail::new(e.to_string())));
+fn parse_num<'a>(input: &'a str) -> IResult<&'a str, f64> {
+    double(input)
 }
 
-fn parse_bool(input: &[u8]) -> IResult<&[u8], bool> {
+fn parse_bool(input: &str) -> IResult<&str, bool> {
     alt((
         map(tag_no_case("true"), |_| true),
-        map(tag_no_case("false"), |_| false)
+        map(tag_no_case("false"), |_| false),
     ))(input)
 }
 
+#[test]
+fn bool() {
+    let json = " truefalse";
+    let out = JsonParser::parse(json);
+    assert!(out.is_ok())
+}
