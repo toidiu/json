2fbe9be1a8202a746047970c022eefec9ba5dc23
toidiu <apoorv@toidiu.com>
2020-08-17 00:26:02 -0700

    add tests for other types

--- src/lib.rs
+++ src/lib.rs
@@ -35,29 +35,74 @@ pub struct JsonParser<'a> {
 
 impl<'a> JsonParser<'a> {
     pub fn parse(mut json: &'a str) -> IResult<&'a str, bool> {
-        preceded(parse_space, parse_bool)(json)
+        context(
+            "parse",
+            preceded(parse_space, delimited(parse_arr, parse_bool, parse_arr)),
+        )(json)
     }
+}
+
+fn parse_arr<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
+    let chars = "[]";
+    context("arr", take_while(move |c| chars.contains(c)))(i)
 }
 
 fn parse_space<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
     let chars = " \t\r\n";
-    take_while(move |c| chars.contains(c))(i)
+    context("space", take_while(move |c| chars.contains(c)))(i)
 }
 
 fn parse_num<'a>(input: &'a str) -> IResult<&'a str, f64> {
-    double(input)
+    context("space", double)(input)
 }
 
 fn parse_bool(input: &str) -> IResult<&str, bool> {
-    alt((
-        map(tag_no_case("true"), |_| true),
-        map(tag_no_case("false"), |_| false),
-    ))(input)
+    context(
+        "bool",
+        alt((
+            map(tag_no_case("true"), |_| true),
+            map(tag_no_case("false"), |_| false),
+        )),
+    )(input)
 }
 
-#[test]
-fn bool() {
-    let json = " truefalse";
-    let out = JsonParser::parse(json);
-    assert!(out.is_ok())
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_bool() {
+        let mut json = "true";
+        let mut out = parse_bool(json);
+        assert_eq!(out.unwrap().1, true);
+
+        json = "false";
+        out = parse_bool(json);
+        assert_eq!(out.unwrap().1, false);
+
+        json = "afalse";
+        out = parse_bool(json);
+        assert!(out.is_err());
+    }
+
+    #[test]
+    fn test_number() {}
+
+    #[test]
+    fn test_object() {}
+
+    #[test]
+    fn test_array() {}
+
+    #[test]
+    fn test_string() {}
+
+    #[test]
+    fn test_parser() {
+        let json = " [true, false] ";
+        let out = JsonParser::parse(json);
+        println!("======={:?}", out);
+        assert_eq!(out.unwrap().1, true)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use json;
 
 fn main() {
     // get input
-    let json = "truefalse";
+    let json = " [ true, false] ";
     let out = json::JsonParser::parse(json);
     println!("{:?}", out);
 }
